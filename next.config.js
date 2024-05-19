b2cd8f5abcafb4dcd516978801c005373df2b16b
muzzzkhan <muzammil.ah14@gmail.com>
2024-05-19 21:00:58 +0500

    Reverting Typescript checks disable

--- next.config.js
+++ next.config.js
@@ -10,14 +10,7 @@ const nextConfig = {
 				protocol: "https",
 				hostname: "PolyWalk-production.up.railway.app",
 			},
-		],
-		typescript: {
-			// !! WARN !!
-			// Dangerously allow production builds to successfully complete even if
-			// your project has type errors.
-			// !! WARN !!
-			ignoreBuildErrors: true,
-		},
+		]
 	},
 };
 
